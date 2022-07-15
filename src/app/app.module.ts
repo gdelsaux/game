0997506f02669ab3aabd5da917c771bac5948f90
gdelsaux <gaetandelsaux@gmail.com>
2022-07-15 14:49:27 +0100

    creation of the components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GameControlsComponent } from './game-controls/game-controls.component';
+import { OddComponent } from './odd/odd.component';
+import { EvenComponent } from './even/even.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    GameControlsComponent,
+    OddComponent,
+    EvenComponent,
   ],
   imports: [
     BrowserModule,
